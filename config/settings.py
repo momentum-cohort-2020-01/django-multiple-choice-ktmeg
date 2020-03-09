c2676597189d5f345f6076a17ff79dd369fb711f
Kat <kmneil2@gmail.com>
2020-03-09 17:28:25 -0400

    start

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'registration'
+    'registration',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- users/models.py
+++ users/models.py
@@ -6,19 +6,4 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    
-user_name = models.CharField(max_length=50)
-email = models.EmailField(max_length=100)
-password = models.CharField(max_length=40)
-image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
-
-
-def __str__(self):
-    return self.user_name
-
-
-class Snippet(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    title = models.CharField(max_length=50)
-    description = models.CharField(max_length=200)
-    code = models.
+    pass
