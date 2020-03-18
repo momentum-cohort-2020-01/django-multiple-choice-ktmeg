8bddeef39e803d089dfb958d82ca5873c7773d2c
Kat <kmneil2@gmail.com>
2020-03-18 19:43:41 -0400

    snips.js add function, not working

--- static/js/snips.js
+++ static/js/snips.js
@@ -7,14 +7,32 @@ function q (selector) {
 }
 
 function main () {
-  new ClipboardJS('.btn')
-  const button = q('btn.copy')
-  button.addEventListener('click', function (e) {
-    e.preventDefault()
-  })
-
+  const buttons = document.querySelectorAll('.clipboard')
+  for (let button of buttons) {
+    button.addEventListener('click', function (e) {
+    // e.preventDefault()
+      const parent = e.target.closest('.code-snip')
+      const snippet = parent.q('code')
+      const range = document.createRange()
+      range.selectNode(snippet)
+      window.getSelection().removeAllRanges()
+      window.getSelection().addRange(range)
+      try {
+        const successful = document.execCommand('copy')
+        const msg = successful ? 'successful' : 'unsuccessful' 
+      } catch (err) {
+        console.log('Cannot copy snippet!')
+      } window.getSelection().removeAllRanges()
+    })
+  }
 }
+function copy() {
+  main()
+}
+console.log('test')
+document.addEventListener('DOMContentLoaded', copy())
 
+console.log('complete')
 // function copy (event) {
 //   newSnip()
 // }
